ccef4102480bf909267ee6149af779ebef922267
Damian Kwasny <damiankwasny95@gmail.com>
2019-11-09 11:53:01 +0100

    add setup file to allow installing as a package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='mouse_disambiguator',
+   version='0.1',
+   description='Package for differentiaiting between animal and computer device context for word mouse',
+   author='Damian Kwasny',
+   author_email='damiankwasny95@gmail.com',
+   packages=find_packages()
+)
